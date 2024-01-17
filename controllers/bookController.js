38e64eefd4ec031e9ebe677d4b6f02be16e9012b
PythonXHunter <aryan.computerscience@gmail.com>
2024-01-17 18:57:44 +0530

    set the all books view and required methods in bookController

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -34,7 +34,15 @@ const index = asyncHandler(async (req, res, next) => {
 });
 
 const book_list = asyncHandler(async (req, res, next) => {
-  res.send("Not IMPLEMENTED: Book List");
+  const allBooks = await Book.find({}, "title author")
+    .sort({title: 1})
+    .populate("author")
+    .exec();
+
+  res.render("book_list", {
+    title: "Book List",
+    book_list: allBooks
+  });
 });
 
 const book_detail = asyncHandler(async (req, res, next) => {
